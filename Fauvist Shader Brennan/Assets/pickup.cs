4f22f4220e090f5f94fe007c780e4c22300e9771
Edmund Liang <eliang5@ucsc.edu>
2020-06-09 19:42:45 -0700

    pickup2 works properly

---
+++ Fauvist Shader Brennan/Assets/pickup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class pickup : MonoBehaviour
+{
+    public Transform onHand;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+        this.transform.position = onHand.transform.position;
+        this.transform.parent = GameObject.Find("FirstPerson-AIO").transform;
+    }
+
+    void OnMouseUp()
+    {
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+}
